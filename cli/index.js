a8c7fb337996c252e1c59dc3ce345ee596f4ef30
Christophe Tiraoui <krux@krux.fr>
2022-10-22 17:43:53 +0200

    💚 ci

--- cli/index.js
+++ cli/index.js
@@ -1,11 +1,25 @@
 #!/usr/bin/env node
 
-const fs = require("fs");
 const path = require("path");
+const fs = require("fs");
+const childProc = require("child_process");
 
 const ludivinePackageJsonFile = path.resolve(__dirname, "..", "package.json");
 const ludivinePackageJson = JSON.parse(
   fs.readFileSync(ludivinePackageJsonFile, { encoding: "utf-8" })
 );
 
-console.log(`Awesome Command ${ludivinePackageJson.version}`);
+console.info(`Ludivine ${ludivinePackageJson.version}`);
+
+const ps = childProc.exec("npm exec --yes ts-node " + ludivinePackageJson.main);
+process.stdin.pipe(ps.stdin);
+ps.stdout.pipe(process.stdout);
+
+ps.on("exit", (code) => {
+  console.info("ended with code", code);
+  process.exit(code);
+});
+ps.on("error", (err) => {
+  console.error("ended with error", err);
+  process.exit(1);
+});
