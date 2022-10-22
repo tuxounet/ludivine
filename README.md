8244fc894e3dc745c2c552e3ce2c8b50cf38a192
Christophe Tiraoui <krux@krux.fr>
2022-10-22 17:05:32 +0200

    🧑‍💻 usage and cli preparation

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # @tuxounet/ludivine
 
 Ludivine, a(n individual) virtual digital assistant
+
+# USAGE
+
+## via npx
+
+```console
+npx --package=github:tuxounet/ludivine#main ludivine
+# or
+npm exec --package=github:tuxounet/ludivine#main ludivine
+```
--- cli/index.js
+++ cli/index.js
@@ -1,3 +1,11 @@
 #!/usr/bin/env node
 
-console.log("Awesome Command 0.1.2");
+const fs = require("fs");
+const path = require("path");
+
+const ludivinePackageJsonFile = path.resolve(__dirname, "..", "package.json");
+const ludivinePackageJson = JSON.parse(
+  fs.readFileSync(ludivinePackageJsonFile, { encoding: "utf-8" })
+);
+
+console.log(`Awesome Command ${ludivinePackageJson.version}`);
